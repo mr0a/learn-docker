e3cbee8f52755a42221af042b19a55fa9de5424e
mr0a <55923312+mr0a@users.noreply.github.com>
2025-07-10 11:38:57 +0530

    add nodejs app with docker file

---
+++ app/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { logger } from "./logger.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  logger.info("Received a request on /");
+  res.send("Hello, World!");
+});
+
+app.get("/:value", (req, res) => {
+  const value = req.params.value;
+  logger.info("Received " + value + " from URL");
+  res.send("Hello " + value);
+});
+
+const port = process.env.SERVER_PORT || 8080;
+const dbHost = process.env.DBHOST || localhost;
+app.listen(port, async () => {
+  logger.info("Starting the server...");
+  await connectDB();
+  logger.info("Listening on port " + port);
+  logger.info("Server started successfully");
+});
+
+async function connectDB() {
+  logger.info("Connecting to MongoDB...");
+  try {
+    await mongoose.connect(`mongodb://${dbHost}:27017/dummy`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.info("Connected to MongoDB successfully");
+  } catch (error) {
+    logger.error("Error connecting to MongoDB: ", error);
+  }
+  logger.info("Connected to MongoDB...");
+  console.log(
+    "Server Started Successfully. See log file for more detailed info"
+  );
+}
